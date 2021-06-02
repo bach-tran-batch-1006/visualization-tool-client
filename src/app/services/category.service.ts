c91876878a7f2525019c57e61c00bf0338baec11
Chakong Xiong <33104327+CommanderXiong@users.noreply.github.com>
2021-06-02 16:27:53 -0400

    Initial commit

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -7,10 +7,26 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CategoryService {
 
-  apiURL = 'http://3.226.243.38:8081';
+  apiURL = 'http://3.226.243.38:8081/category/';
 
   constructor(private httpClient: HttpClient) { }
 
+  getCategories(){
+    return this.httpClient.get<Object[]>(this.apiURL);
+  }
 
+  addCategory(bodyObject: object){
+    return this.httpClient.post<Object[]>(this.apiURL,
+    bodyObject);
+  }
 
+  updateCategory(id: number, bodyObject: object){
+    return this.httpClient.put<Object[]>(this.apiURL+id,
+    bodyObject);
+  }
+
+  deleteCategory(id: number, bodyObject: object){
+    return this.httpClient.delete<Object[]>(this.apiURL+id,
+    bodyObject);
+  }
 }

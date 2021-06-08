2452e0c6e131981c8ed8a6efe344f365cb9390d6
hpatel183 <hmpatel183@outlook.com>
2021-06-08 17:21:11 -0500

    Add Skill-Category Protractor test cases

--- e2e/src/SkillsAndCategories.e2e-spec.ts
+++ e2e/src/SkillsAndCategories.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { browser, logging } from 'protractor';
+import { browser, logging, by, element } from 'protractor';
 import { SkillsAndCategoriesPage } from './SkillsAndCategories.po';
 
 describe('Skills and Categories Edit Page', () => {
@@ -8,20 +8,49 @@ describe('Skills and Categories Edit Page', () => {
         page = new SkillsAndCategoriesPage();
     });
 
-    // it('should display welcome message', async () => {
-    //     await page.navigateTo();
-    //     expect(await page.getTitleText()).toEqual('Project3Cvt');
-    // });
+    it('should add a new category and click submit and verify it was added', async () => {
+        await browser.get('http://localhost:4200/edit/skill-category');
+        await page.clickShowAddCategoryButton();
+        await page.newCategoryInput();
+        await page.newCategoryTextarea();
+        await page.clickSubmitAddCategoryButton();
+
+        expect(await page.getNewCategoryRadioText()).toEqual("ZZZ-Category");
+    });
+
+    it('should add a new skill and click submit and verify it was added', async () => {
+        await browser.get('http://localhost:4200/edit/skill-category');
+        await page.clickShowAddSkillButton();
+        await page.checkCategory();
+        await page.newSkillInput();
+        await page.clickSubmitAddSkillButton();
+
+        expect(await page.getNewSkillRadioText()).toEqual('ZZZ-Testing');
+    });
+
+    it('should select, click delete and remove last added skill "ZZZ-Testing"', async () => {
+        await browser.get('http://localhost:4200/edit/skill-category');
+        await page.checkSkillToDelete();
+        await page.clickDeleteSkillButton();
+
+        expect(await page.getLastSkillRadioText()).not.toEqual("ZZZ-Testing");
+    });
+
+    it('should select, click delete and remove last added category "ZZZ-Category"', async () => {
+        await browser.get('http://localhost:4200/edit/skill-category');
+        await page.checkCategoryToDelete();
+        await page.clickDeleteCategoryButton();
+
+        expect(await page.getLastCategoryRadioText()).not.toEqual("ZZZ-Category");
+    });
 
 
 
-
-
-    // afterEach(async () => {
-    //     // Assert that there are no errors emitted from the browser
-    //     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    //     expect(logs).not.toContain(jasmine.objectContaining({
-    //         level: logging.Level.SEVERE,
-    //     } as logging.Entry));
-    // });
+    afterEach(async () => {
+        // Assert that there are no errors emitted from the browser
+        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+        expect(logs).not.toContain(jasmine.objectContaining({
+            level: logging.Level.SEVERE,
+        } as logging.Entry));
+    });
 });

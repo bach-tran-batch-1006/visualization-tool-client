9f0ebe0df74fb0833bc75f0670139ae43a0f95d6
Gentry Hunt <ghunt@csu.edu>
2021-09-02 16:59:16 -0500

    generated profile account component.. needs css

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  
   logout():void{
     localStorage.clear();
     sessionStorage.clear();
